b4b6bb93770ee20967666d4102b4d6b9129d1f76
Lucia <54008196+lucia160292@users.noreply.github.com>
2019-09-27 09:11:22 -0300

    creación

---
+++ datos.js
@@ -0,0 +1,29 @@
+var usuario = null;
+function guardarDatos(){
+  let nombre = document.getElementById('nombre').value;
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  usuario = {
+    "nombre" : nombre,
+    "email" : email,
+    "password" : password,
+  };
+  localStorage.setItem("usuario", JSON.stringify(usuario));
+  window.location.href = 'index.html';
+  console.log(usuario);
+
+}
+function getUsuario() {
+  let usuario = JSON.parse(localStorage.getItem("usuario"));
+  console.log(usuario);
+  document.getElementById("nombre").innerText = usuario.nombre;
+}
+function getPerfil() {
+  let usuario = JSON.parse(localStorage.getItem("usuario"));
+  document.getElementById("nombre").innerText = usuario.nombre;
+  document.getElementById("email").innerText = usuario.email;
+}
+function logout() {
+  localStorage.removeItem("usuario");
+  window.location.href = "login.html";
+}
