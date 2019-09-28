05f7f24a5c6a7a3f36bc69c9ab8bf9b18a970fb2
Lucia <54008196+lucia160292@users.noreply.github.com>
2019-09-28 09:45:44 -0300

    ww

--- datos.js
+++ datos.js
@@ -40,6 +40,7 @@ function onSignIn(googleUser) {
   if (auth2.isSignedIn.Ab === true){
     window.location.href = "https://lucia160292.github.io/e-commerce../";
   }
+  
   let usuario = profile.getEmail();
     sessionStorage.setItem('usuario', usuario);
     signOut();
