0bb67faf27a41f5fa08f1e195785e7229bbd1ae2
Lucia <54008196+lucia160292@users.noreply.github.com>
2019-09-28 11:03:35 -0300

    aa

--- datos.js
+++ datos.js
@@ -1,10 +1,10 @@
 
 var usuario = null;
-let nombre = document.getElementById('name').value;
+let nombre = document.getElementById('nombre').value;
 let email = document.getElementById('email').value;
 let password = document.getElementById('password').value;
 usuario = {
-  "name" : name,
+  "nombre" : name,
   "email" : email,
   "password" : password,
 };
@@ -17,11 +17,11 @@ console.log(usuario);
 function getUsuario() {
 let usuario = JSON.parse(localStorage.getItem("usuario"));
 console.log(usuario);
-document.getElementById("name").innerText = usuario.nombre;
+document.getElementById("nombre").innerText = usuario.nombre;
 }
 function getPerfil() {
 let usuario = JSON.parse(localStorage.getItem("usuario"));
-document.getElementById("name").innerText = usuario.nombre;
+document.getElementById("nombre").innerText = usuario.nombre;
 document.getElementById("email").innerText = usuario.email;
 }
 function logout() {
