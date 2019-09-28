6528338616103c7193cb765566b57fe9a0e5658e
Lucia <54008196+lucia160292@users.noreply.github.com>
2019-09-28 09:53:33 -0300

    datos2

--- datos.js
+++ datos.js
@@ -30,28 +30,7 @@ window.location.href = "login.html";
 }
 
     
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  var auth2 = gapi.auth2.getAuthInstance();
-  if (auth2.isSignedIn.Ab === true){
-    window.location.href = "https://lucia160292.github.io/e-commerce../";
-  }
 
-  let usuario = profile.getEmail();
-    sessionStorage.setItem('usuario', usuario);
-    signOut();
-
-}
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
 
 
   
